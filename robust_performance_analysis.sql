0c34a8c4a4ecce425dd4ee4959a74f1aeb97446f
chili74 <onthakgile.chiloane@gmail.com>
2026-08-10 21:38:13 +0200

    Update robust_performance_analysis.sql

--- robust_performance_analysis.sql
+++ robust_performance_analysis.sql
@@ -17,9 +17,15 @@ product_name,
 current_sales,
 avg(current_sales) over (partition by product_name) as avg_sale,
 current_sales - avg(current_sales) over (partition by product_name) as avg_diff,
-case when current_sales - avg(current_sales) over (partition by product_name) > 0 then 'above average' 
-     when current_sales - avg(current_sales) over (partition by product_name) < 0 then 'below average' 
+case when current_sales - avg(current_sales) over (partition by product_name) > 0 then 'Above average' 
+     when current_sales - avg(current_sales) over (partition by product_name) < 0 then 'Below average' 
      else 'average' 
-end sales_performance
+end avg_change,
+lag(current_sales) over(partition by product_name order by order_year) py_sales,
+current_sales - lag(current_sales) over (partition by product_name order by order_year) as diff_py,
+case when current_sales - lag(current_sales) over (partition by product_name order by order_year) > 0 then 'Increase'
+     when current_sales - lag(current_sales) over (partition by product_name order by order_year) < 0 then 'Decrease'
+     else 'No Change'
+end py_avg_change
 from yearly_product_sales
 order by product_name, order_year;
